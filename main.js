ae6eade5b613bb386097667172c987eb6f84bb20
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2023-05-09 11:44:43 +0700

    init

---
+++ main.js
@@ -0,0 +1,2 @@
+require("./sort/insertion-sort")
+
---
+++ sort/insertion-sort.js
@@ -0,0 +1,11 @@
+let arr = [6, -3, 10, 2, -7, 4, 1, 8, -5];
+
+function insertionSort(arr) {
+    // Insertion sort code here
+
+    return arr;
+}
+
+let result = insertionSort(arr);
+
+console.log(result);
