2ad0e05e92955f2ad257e31971e6a5033989a61b
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2023-05-10 08:52:20 +0700

    Insertion Sort

--- sort/insertion-sort.js
+++ sort/insertion-sort.js
@@ -1,11 +1,18 @@
 let arr = [6, -3, 10, 2, -7, 4, 1, 8, -5];
 
 function insertionSort(arr) {
-    // Insertion sort code here
+	// Insertion sort code here
+	for (let i = 1; i < arr.length; i++) {
+		let key = arr[i];
+		let j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			arr[j] = key;
+			j--;
+		}
+	}
 
-    return arr;
+	return arr;
 }
 
-let result = insertionSort(arr);
-
-console.log(result);
+console.log(insertionSort(arr));
